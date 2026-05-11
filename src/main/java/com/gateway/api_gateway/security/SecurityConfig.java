513bf9a475d70f26c69bc04b4cf4fce4c498369d
Cri008d <cri.zunigam@duocuc.cl>
2026-05-10 23:35:59 -0400

    Add files via upload

---
+++ src/main/java/com/gateway/api_gateway/security/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.gateway.api_gateway.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+            .csrf(csrf -> csrf.disable()) // Desactivar CSRF para permitir POST
+            .authorizeExchange(exchanges -> exchanges
+                .anyExchange().permitAll() // Permitir todo el tráfico
+            );
+        return http.build();
+    }
+}
